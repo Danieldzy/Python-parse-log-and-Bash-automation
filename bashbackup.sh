f4092c51ae7b9c60429453eb6deb0653daf62da5
Daniel Deng <165398435+Danieldzy@users.noreply.github.com>
2024-07-20 19:23:34 -0600

    Bash script for file backup

---
+++ bashbackup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#define the file that need backup and the backup directory
+file="/home/student/member_data.txt"
+dir="/home/student/Autobackup"
+
+#add time stamp to the member_data file so each time of backup 
+#a new time stamped file will appear
+newfile="$dir/member_data_$(date +%Y%m%d_%H%M%S).txt"
+
+# Copy the file to the backup directory and rename to newfile with time stamp
+cp "$file" "$newfile"
+
+# Print a message indicating the backup was successful
+echo "Backup of $file completed and saved as $newfile"
